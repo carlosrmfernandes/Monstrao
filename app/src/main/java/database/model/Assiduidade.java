f2566c08b384e78c09d7670576e11ad3e39ff4ee
comp8 <comp8@lab.unesc.net>
2019-04-11 20:46:08 -0300

    Criei o mondel, Assiduidade, FaturasMatriculas, Modalidades e Usuarios

--- app/src/main/java/database/model/Assiduidade.java
+++ app/src/main/java/database/model/Assiduidade.java
@@ -1,4 +1,22 @@
 package database.model;
 
-public class Cidades {
+public class Assiduidade {
+    private int codigo_matricula;
+    private String data_entrada;
+
+    public int getCodigo_matricula() {
+        return codigo_matricula;
+    }
+
+    public void setCodigo_matricula(int codigo_matricula) {
+        this.codigo_matricula = codigo_matricula;
+    }
+
+    public String getData_entrada() {
+        return data_entrada;
+    }
+
+    public void setData_entrada(String data_entrada) {
+        this.data_entrada = data_entrada;
+    }
 }
