dec2c5903c900eca3aebf62308ca9f89043c8e34
comp8 <comp8@lab.unesc.net>
2019-04-11 21:25:24 -0300

    criando os model Aluno, Cidades, Matricula

--- app/src/main/java/database/model/Alunos.java
+++ app/src/main/java/database/model/Alunos.java
@@ -1,4 +1,149 @@
 package database.model;
 
 public class Alunos {
+    private  String codigo_aluno;
+    private  String nome;
+    private  String data_nascimento;
+    private  String sexo;
+    private  String telefone;
+    private  String celular ;
+    private  String email;
+    private  String observacao;
+    private  String endereco;
+    private  String numero;
+    private  String complemento;
+    private  String bairro;
+    private  int cidade;
+    private  int estado;
+    private  int pais;
+    private  String cep;
+
+
+    public String getCodigo_aluno() {
+        return codigo_aluno;
+    }
+
+    public void setCodigo_aluno(String codigo_aluno) {
+        this.codigo_aluno = codigo_aluno;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getData_nascimento() {
+        return data_nascimento;
+    }
+
+    public void setData_nascimento(String data_nascimento) {
+        this.data_nascimento = data_nascimento;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getCelular() {
+        return celular;
+    }
+
+    public void setCelular(String celular) {
+        this.celular = celular;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getObservacao() {
+        return observacao;
+    }
+
+    public void setObservacao(String observacao) {
+        this.observacao = observacao;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getComplemento() {
+        return complemento;
+    }
+
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public int getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(int cidade) {
+        this.cidade = cidade;
+    }
+
+    public int getEstado() {
+        return estado;
+    }
+
+    public void setEstado(int estado) {
+        this.estado = estado;
+    }
+
+    public int getPais() {
+        return pais;
+    }
+
+    public void setPais(int pais) {
+        this.pais = pais;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
 }
--- app/src/main/java/database/model/Cidades.java
+++ app/src/main/java/database/model/Cidades.java
@@ -1,4 +1,32 @@
 package database.model;
 
 public class Cidades {
+    private String cidade;
+    private String estado;
+    private String pais;
+
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getPais() {
+        return pais;
+    }
+
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
 }
