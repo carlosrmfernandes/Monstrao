2a129438c696c46ca7645466e55f036133099eb6
comp8 <sfadsafasf@gmail.com>
2019-04-25 20:14:52 -0300

    criando classes DAO

--- app/src/main/java/database/dao/AbstrataDAO.java
+++ app/src/main/java/database/dao/AbstrataDAO.java
@@ -12,7 +12,6 @@ public abstract class AbstrataDAO {
 
     protected  final void close(){
 
-
     }
 
 }
---
+++ app/src/main/java/database/dao/AssiduidadeDAO.java
@@ -0,0 +1,28 @@
+package database.dao;
+
+import android.content.Context;
+
+public class AssiduidadeDAO extends  AbstrataDAO {
+
+    private final String [] colunas =
+            {
+
+            };
+
+    public AssiduidadeDAO (Context ao_Context){
+
+    }
+
+    public int Insert(){
+        return 0;
+    }
+    public int Delete(){
+        return 0;
+    }
+    public int Update(){
+        return 0;
+    }
+    /*public int List<AssiduidadeModel> Select(){
+        return null;
+    }*/
+}
