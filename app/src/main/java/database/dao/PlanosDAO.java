5426b6ba7dabd973518f54043ad809b06ce20918
comp8 <kakakakak@gmail.com>
2019-04-25 20:15:01 -0300

    create class DAO and class for conection in db.

---
+++ app/src/main/java/database/dao/PlanosDAO.java
@@ -0,0 +1,37 @@
+package database.dao;
+
+
+import android.content.Context;
+
+import java.util.List;
+
+import database.model.Modalidades;
+import database.model.Planos;
+
+public class PlanosDAO extends AbstrataDAO{
+
+    private final String[]
+    colunas = {
+
+    };
+
+    public PlanosDAO(Context context){
+
+    }
+
+    public int Insert(){
+        return 0;
+    }
+
+    public int Delete(){
+        return 0;
+    }
+
+    public int Update(){
+        return 0;
+    }
+
+    public List<Planos> Select(){
+        return null;
+    }
+}
