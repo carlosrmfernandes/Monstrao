7f0d1bcb7ef64d34f4ede88509488828cd63d8ba
comp8 <sfadsafasf@gmail.com>
2019-04-25 19:53:47 -0300

    criando classe abstrata

---
+++ app/src/main/java/database/dao/AbstrataDAO.java
@@ -0,0 +1,18 @@
+package database.dao;
+
+import android.database.sqlite.SQLiteDatabase;
+
+public abstract class AbstrataDAO {
+
+    protected SQLiteDatabase db;
+
+    protected  final void open(){
+
+    }
+
+    protected  final void close(){
+
+
+    }
+
+}
